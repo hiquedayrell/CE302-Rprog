186cb669d0dc57df2637629c9a65453e5e813a20
hiquedayrell <henriquedayrell@ufpr.br>
2025-08-27 19:02:12 -0300

    teste

---
+++ R4DS.R
@@ -0,0 +1,14 @@
+library(tidyverse) #para instalar, utilizar install.packages("tidyverse"), toda vez que for usar, library(tidyverse)
+ggplot2::mpg # ggplot2:: "chama a funcao gg plot", o mpg seria o conjunto de dados sobre veiculos
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) #displ = capacidade do motor, hwy = numero de milhas por galao na estrada
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = cyl, y = hwy ))
+
+#ggplot(data = <DATA>) + 
+#<GEOM_FUNCTION>(mapping = aes(<MAPPINGS))
+ 
+ library(usethis)
+use_git_config(user.name = "hiquedayrell",
+               user.email = "henriquedayrell@ufpr.br")
