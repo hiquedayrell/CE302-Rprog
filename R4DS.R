eaffccf3b0528c41be300fec93cfadebad6fdc55
hiquedayrell <henriquedayrell@ufpr.br>
2025-09-19 19:07:11 -0300

    teste

--- R4DS.R
+++ R4DS.R
@@ -3,10 +3,8 @@ ggplot2::mpg # ggplot2:: "chama a funcao gg plot", o mpg seria o conjunto de dad
 
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy)) #displ = capacidade do motor, hwy = numero de milhas por galao na estrada
-ggplot(data = mpg) + 
-  geom_point(mapping = aes(x = cyl, y = hwy ))
+
 
 #ggplot(data = <DATA>) + 
 #<GEOM_FUNCTION>(mapping = aes(<MAPPINGS))
  
-#teste de alterações
